57e3832cc2188fa878612752dd96fc645b2cfe01
nikolam <nikola.mosurovic@providus.rs>
2023-01-28 19:20:40 +0100

    add resource

--- main.tf
+++ main.tf
@@ -6,3 +6,14 @@ resource "aws_vpc" "test_vpc" {
     Name = "dev"
   }
 }
+
+resource "aws_subnet" "test_public_subnet" {
+  vpc_id = aws_vpc.test_vpc.id
+  cidr_block = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
