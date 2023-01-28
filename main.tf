25a93979630d28f167983b96694aca43f521f4a6
nikolam <nikola.mosurovic@providus.rs>
2023-01-28 20:11:17 +0100

    add resource aws_internet_gateway

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "test_vpc" {
-  cidr_block = "10.123.0.0/16"
+  cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
 
   tags = {
@@ -8,12 +8,20 @@ resource "aws_vpc" "test_vpc" {
 }
 
 resource "aws_subnet" "test_public_subnet" {
-  vpc_id = aws_vpc.test_vpc.id
-  cidr_block = "10.123.1.0/24"
+  vpc_id                  = aws_vpc.test_vpc.id
+  cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "eu-central-1a"
+  availability_zone       = "eu-central-1a"
 
   tags = {
     Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "test_internet_gateway" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = "dev-igw"
   }
 }
