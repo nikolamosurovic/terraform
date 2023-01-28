47a1c6cf5421396878e0ed07b491fe2b2adf8f74
nikolam <nikola.mosurovic@providus.rs>
2023-01-28 22:52:25 +0100

    add resource aws_route

--- main.tf
+++ main.tf
@@ -25,3 +25,17 @@ resource "aws_internet_gateway" "test_internet_gateway" {
     Name = "dev-igw"
   }
 }
+
+resource "aws_route_table" "test_public_rt" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = "dev-public-rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.test_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.test_internet_gateway.id
+}
