27f9d9bc61ad6c9d3b8f0480a8c6c718d1889eb0
nikolam <nikola.mosurovic@providus.rs>
2023-01-31 12:45:04 +0100

    add resource aws_route

--- main.tf
+++ main.tf
@@ -39,3 +39,8 @@ resource "aws_route" "default_route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.test_internet_gateway.id
 }
+
+resource "aws_route_table_association" "nas_public_assoc" {
+  route_table_id = aws_route_table.test_public_rt.id
+  subnet_id      = aws_subnet.test_public_subnet.id
+}
